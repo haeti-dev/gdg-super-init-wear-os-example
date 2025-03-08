b6d87f82e5e58c4b7d4cfbadcb286c0aca4cc957
HaeTi <t9h9@naver.com>
2025-03-08 22:44:23 +0900

    add : TimeTextExample

--- app/src/main/java/com/haeti/wearsample/presentation/TimeText.kt
+++ app/src/main/java/com/haeti/wearsample/presentation/TimeText.kt
@@ -1,1 +1,22 @@
 package com.haeti.wearsample.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.TimeText
+import androidx.wear.compose.material.TimeTextDefaults
+import androidx.wear.compose.ui.tooling.preview.WearPreviewDevices
+import androidx.wear.compose.ui.tooling.preview.WearPreviewFontScales
+
+@Composable
+fun TimeTextExample() {
+	TimeText(
+		timeSource = TimeTextDefaults.timeSource(timeFormat = "HH:mm:ss"),
+		timeTextStyle = TimeTextDefaults.timeTextStyle()
+	)
+}
+
+@WearPreviewDevices
+@WearPreviewFontScales
+@Composable
+fun TimeExamplePreview() {
+	TimeTextExample()
+}
